35c2903abd71615a988c9f2a568555fe48b62026
todatoday <taki.souad-90@hotmail.com>
2025-05-30 10:08:42 +0200

    type style

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,34 +1,39 @@
-'use client';
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
 
 // for the active classe
-import { usePathname } from 'next/navigation'; //can not do it on the srever
+import { usePathname } from "next/navigation"; //can not do it on the srever
 
 interface NavLinkType {
-    name: string,
-    url: string
+  name: string;
+  url: string;
 }
 
-const navLinks:NavLinkType[] = [
-    {name: "home" , url: "/"},
-    {name: "pokemon", url: "/pokemon"},
-    {name: "types", url: "/types"},
-    {name: "about", url: "/about"}
-]
+const navLinks: NavLinkType[] = [
+  { name: "home", url: "/" },
+  { name: "pokemon", url: "/pokemon" },
+  { name: "types", url: "/types" },
+  { name: "about", url: "/about" },
+];
 
 const Navigation = () => {
-    const path = usePathname();
-    // for class active
-    return(
-        <nav className='menu'>
-            {navLinks.map((item:NavLinkType, index:number) =>
-                 <Link key={index}
-                className={`menu__item--${item.name} ${path === item.url ? "active" : ""}`}
-                href={item.url}>
-                    {item.name}
-                </Link>)}
-        </nav>
-    )
-}
+  const path = usePathname();
+  // for class active
+  return (
+    <nav className="menu">
+      {navLinks.map((item: NavLinkType, index: number) => (
+        <Link
+          key={index}
+          className={`menu__item--${item.name} ${
+            path === item.url ? "active" : ""
+          }`}
+          href={item.url}
+        >
+          {item.name}
+        </Link>
+      ))}
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
